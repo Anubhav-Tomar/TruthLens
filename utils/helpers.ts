95c9ff363f3950c9d04c188af512d9e2ad30d07b
Anubhav-Tomar <122211486+Anubhav-Tomar@users.noreply.github.com>
2025-11-08 01:08:08 +0530

    Update helpers.ts

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,3 @@
 export const isDev = process.env.NODE_ENV === 'development';
 
-export const ORIGIN_URL = isDev ? 'http://localhost:3000' : 'https://sum-ai-azure.vercel.app/';
+export const ORIGIN_URL = isDev ? 'http://localhost:3000' : 'https://truth-lens-rouge.vercel.app/';
